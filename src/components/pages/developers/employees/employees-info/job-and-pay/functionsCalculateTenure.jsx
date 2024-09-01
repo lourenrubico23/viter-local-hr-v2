d1fad591c863c2070f5315a3ca2d0de8045186d8
unknown <lourenisobel18@gmail.com>
2024-09-01 12:40:32 +0800

    tenure fixed

--- src/components/pages/developers/employees/employees-info/job-and-pay/functionsCalculateTenure.jsx
+++ src/components/pages/developers/employees/employees-info/job-and-pay/functionsCalculateTenure.jsx
@@ -1,39 +1,39 @@
 import React from "react";
 
 const functionsCalculateTenure = () => {
-  const [hireDate, setHireDate] = React.useState("");
-  const [tenure, setTenure] = React.useState({ years: 0, months: 0 });
+  // const [hireDate, setHireDate] = React.useState("");
+  // const [tenure, setTenure] = React.useState({ years: 0, months: 0 });
 
-  // Function to calculate tenure
-  const calculateTenure = (date) => {
-    const currentDate = new Date();
-    const hire = new Date(date);
+  // // Function to calculate tenure
+  // const calculateTenure = (date) => {
+  //   const currentDate = new Date();
+  //   const hire = new Date(date);
 
-    let years = currentDate.getFullYear() - hire.getFullYear();
-    let months = currentDate.getMonth() - hire.getMonth();
+  //   let years = currentDate.getFullYear() - hire.getFullYear();
+  //   let months = currentDate.getMonth() - hire.getMonth();
 
-    if (months < 0) {
-      years--;
-      months += 12;
-    }
+  //   if (months < 0) {
+  //     years--;
+  //     months += 12;
+  //   }
 
-    if (currentDate.getDate() < hire.getDate()) {
-      months--;
-      if (months < 0) {
-        years--;
-        months += 12;
-      }
-    }
-  };
+  //   if (currentDate.getDate() < hire.getDate()) {
+  //     months--;
+  //     if (months < 0) {
+  //       years--;
+  //       months += 12;
+  //     }
+  //   }
+  // };
 
-  // Handle date change
-  const handleDateChange = (e) => {
-    const date = e.target.value;
-    setHireDate(date);
-    setTenure(calculateTenure(date));
-  };
+  // // Handle date change
+  // const handleDateChange = (e) => {
+  //   const date = e.target.value;
+  //   setHireDate(date);
+  //   setTenure(calculateTenure(date));
+  // };
 
-  return { handleDateChange, hireDate, tenure };
+  // return { handleDateChange, hireDate, tenure };
 };
 
 export default functionsCalculateTenure;
